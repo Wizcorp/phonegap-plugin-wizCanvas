5c691934c0f4e651e04bcac5236e6eb6c4ac94a4
jrouault <jrouault@wizcorp.jp>
2014-05-15 11:06:51 +0900

    Remove unused import.

--- example/android/src/jp/wizcorp/phonegap/plugin/wizCanvas/WizCanvas.java
+++ example/android/src/jp/wizcorp/phonegap/plugin/wizCanvas/WizCanvas.java
@@ -43,11 +43,9 @@ import android.widget.FrameLayout;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.net.URLEncoder;
 import java.util.zip.GZIPInputStream;
 
 @SuppressLint("SetJavaScriptEnabled")
